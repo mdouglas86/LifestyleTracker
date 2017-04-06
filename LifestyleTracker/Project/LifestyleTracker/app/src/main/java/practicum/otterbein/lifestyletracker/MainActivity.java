b981d45fd6aa210d3ff494074e3b67088869a808
Yashi <yashi.hu@otterbein.edu>
2017-04-06 09:38:25 -0400

    Created a Diet button and "linked it" to DietActivity

--- LifestyleTracker/Project/LifestyleTracker/app/src/main/java/practicum/otterbein/lifestyletracker/MainActivity.java
+++ LifestyleTracker/Project/LifestyleTracker/app/src/main/java/practicum/otterbein/lifestyletracker/MainActivity.java
@@ -2,51 +2,36 @@ package practicum.otterbein.lifestyletracker;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private android.support.v4.app.Fragment fragment;
-    private FragmentManager fragmentManager;
+    public Button butt1;
+    public void init() {
+        butt1 = (Button) findViewById(R.id.butt1);
+        butt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DietActivity.class));
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    //mTextMessage.setText(R.string.title_dashboard);
-                    fragment = new DietFragment();
-                    return true;
-                case R.id.navigation_dashboard:
-                    //mTextMessage.setText(R.string.title_notifications);
-                    fragment = new ExerciseFragment();
-                    return true;
-                case R.id.navigation_notifications:
-                    //mTextMessage.setText(R.string.title_settings);
-                    fragment = new SettingsFragment();
-                    return true;
             }
-            return false;
-        }
-
-    };
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
